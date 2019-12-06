0e43b71c6a79ce51e2c4d79642493d8026df7af4
P. Envall <petter.envall@gmail.com>
2019-12-06 15:48:05 +0100

    add utility for disabling tests

--- _template/test.js
+++ _template/test.js
@@ -1,6 +1,8 @@
 const { assert } = require("chai");
 const { solver, solver2 } = require("./solution");
 
+const _describe = _it = () => void 0;
+
 describe("assignment X", () => {
 
     describe("level 1", () => {
